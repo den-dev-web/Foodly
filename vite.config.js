9fd3828fbf90aa495297a97daa9330ba9a29eedf
Den <den@debian-x751md.local>
2026-01-15 14:59:25 +0200

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from "path";
 
 export default defineConfig({
   root: "src",
-  base: "./",
+  base: "/Foodly/",
   publicDir: path.resolve(__dirname, "public"),
   css: {
     devSourcemap: true,
